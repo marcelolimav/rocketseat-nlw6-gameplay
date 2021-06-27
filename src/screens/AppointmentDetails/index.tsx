950940ee726fb82ec538363ea9933aa72313867f
marcelolimav <marcelolimav@gmail.com>
2021-06-27 20:59:40 -0300

    fix: complementos da 5 aula

--- src/screens/AppointmentDetails/index.tsx
+++ src/screens/AppointmentDetails/index.tsx
@@ -1,42 +1,90 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BorderlessButton } from 'react-native-gesture-handler';
+import { useRoute } from '@react-navigation/native';
 import { Fontisto as Icon } from '@expo/vector-icons';
 
-import { ImageBackground, Text, View, FlatList } from 'react-native';
+import { 
+  ImageBackground, 
+  Text, 
+  View, 
+  FlatList, 
+  Alert,
+  Share,
+  Platform
+} from 'react-native';
 
-import { theme } from '../../global/styles/theme';
-import BannerImg from '../../assets/banner.png';
-
-import { Background } from '../../components/Background';
+import { AppointmentProps } from '../../components/Appointment';
+import { Member, MemberProps } from '../../components/Member';
 import { ListDivider } from '../../components/ListDivider';
+import { Background } from '../../components/Background';
 import { ListHeader } from '../../components/ListHeader';
 import { ButtonIcon } from '../../components/ButtonIcon';
 import { Header } from '../../components/Header';
-import { Member } from '../../components/Member';
+import { Load } from '../../components/Load';
 
+import BannerImg from '../../assets/banner.png';
+
+import { theme } from '../../global/styles/theme';
+import { api } from '../../services/api';
 import { styles } from './styles';
 
+type Params = {
+  guildSelected: AppointmentProps
+}
+
+type GuildWidget = {
+  id: string;
+  name: string;
+  instant_invite: string;
+  members: MemberProps[];
+  presence_count: number;
+}
+
 export function AppointmentDetails(){
-  const members = [
-    {
-      id: '1',
-      username: 'Marcelo',
-      avatar_url: "https://github.com/marcelolimav.png",
-      status: 'online'
-    },
-    {
-      id: '2',
-      username: 'Rodrigo',
-      avatar_url: "https://github.com/rodrigorgtic.png",
-      status: 'offline'
+  const [widget, setWidget] = useState<GuildWidget>({} as GuildWidget);
+  const [loading, setLoading] = useState(true);
+  const route = useRoute();
+  const { guildSelected } = route.params as Params;
+
+  async function fetchGuildWidget(){
+    try {
+      const response = await api.get(`/guilds/${guildSelected.guild.id}/widget.json`);
+      setWidget(response.data);
+      console.log(widget);
+      
+    } catch (error) { 
+      Alert.alert('Verifique as configurações do servidor. Será que o Widget está habilitado?');
+    } finally {
+      setLoading(false);
     }
-  ]
+  }
+
+  function handleShareInvitation(){
+    const message = Platform.OS === 'ios' 
+      ? `junte-se a ${guildSelected.guild.name}`
+      : widget.instant_invite;
+      
+    Share.share({
+      message,
+      url: widget.instant_invite
+    });
+  }
+
+  useEffect(()=> {
+    fetchGuildWidget();
+  },[]);
+
+  parou no 1:50:17
+
   return(
     <Background>
       <Header 
         title="Detalhes"
         action={
-          <BorderlessButton>
+          guildSelected.guild.owner && 
+          <BorderlessButton
+            onPress={handleShareInvitation}
+          >
             <Icon 
               name="share"
               size={24}
@@ -52,30 +100,37 @@ export function AppointmentDetails(){
       >
         <View style={styles.bannerContent}>
           <Text style={styles.titulo}> 
-            Lendários
+            {guildSelected.guild.name}
           </Text>
 
           <Text style={styles.subtitulo}> 
-            É hoje que vamos chegar ao challenger sem perder uma partida da m10
+            {guildSelected.description}
           </Text>
         </View>  
       </ImageBackground>
+      {
+        loading 
+        ? <Load />
+        : (
+          <>
+            <ListHeader 
+              title="Jogadores"
+              subtitle={`Total ${widget.members.length}`}
+            />
 
-      <ListHeader 
-        title="Jogadores"
-        subtitle="Total 3"
-      />
+            <FlatList 
+              data={widget.members}
+              keyExtractor={item => item.id}
+              ItemSeparatorComponent={() => <ListDivider isCentered/>}
+              style={styles.members}
+              renderItem={( { item } ) => (
+                <Member data={item}/>
+              )}
+            />
 
-      <FlatList 
-        data={members}
-        keyExtractor={item => item.id}
-        ItemSeparatorComponent={() => <ListDivider isCentered/>}
-        style={styles.members}
-        renderItem={( { item } ) => (
-          <Member data={item}/>
-        )}
-      />
-
+          </>
+        )
+      }
       <View style={styles.footer}>
         <ButtonIcon 
           title="Entrar na partida"
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,120 +1,55 @@
-import React, { useState } from 'react';
-import { useNavigation } from '@react-navigation/native';
+import React, { useState, useCallback } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useFocusEffect, useNavigation } from '@react-navigation/native';
 import { View, FlatList } from 'react-native';
 
+import { Appointment, AppointmentProps } from '../../components/Appointment';
 import { CategorySelect } from '../../components/CategorySelect';
-import { Appointment } from '../../components/Appointment';
 import { ListDivider } from '../../components/ListDivider';
 import { Background } from '../../components/Background';
 import { ListHeader } from '../../components/ListHeader';
 import { ButtonAdd } from '../../components/ButtonAdd';
 import { Profile } from '../../components/Profile';
+import { Load } from '../../components/Load';
 
 import { styles } from './styles';
+import { COLLECTION_APPOINTMENTS } from '../../configs/database';
 
 export function Home() {
   const [category, setCategory] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [appointments, setAppointments] = useState<AppointmentProps[]>([]);
 
   const navigation = useNavigation();
-
-  const appointments = [
-    {
-      id: '1',
-      guild: {
-        id: '1',
-        name: 'Lendários',
-        icon: null,
-        owner: true
-      },
-      category: '1',
-      date: '22/06 às 20:40h',
-      description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'
-    },
-    {
-      id: '2',
-      guild: {
-        id: '1',
-        name: 'Lendários',
-        icon: null,
-        owner: false
-      },
-      category: '1',
-      date: '22/06 às 20:40h',
-      description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'
-    },
-    {
-      id: '3',
-      guild: {
-        id: '1',
-        name: 'Lendários',
-        icon: null,
-        owner: false
-      },
-      category: '1',
-      date: '22/06 às 20:40h',
-      description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'
-    },   
-    {
-      id: '4',
-      guild: {
-        id: '1',
-        name: 'Lendários',
-        icon: null,
-        owner: false
-      },
-      category: '1',
-      date: '22/06 às 20:40h',
-      description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'
-    },
-    {
-      id: '5',
-      guild: {
-        id: '1',
-        name: 'Lendários',
-        icon: null,
-        owner: false
-      },
-      category: '1',
-      date: '22/06 às 20:40h',
-      description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'
-    },
-    {
-      id: '6',
-      guild: {
-        id: '1',
-        name: 'Lendários',
-        icon: null,
-        owner: false
-      },
-      category: '1',
-      date: '22/06 às 20:40h',
-      description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'
-    } ,
-    {
-      id: '7',
-      guild: {
-        id: '1',
-        name: 'Lendários',
-        icon: null,
-        owner: false
-      },
-      category: '1',
-      date: '22/06 às 20:40h',
-      description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'
-    }   
-  ]
 
   function handleCategorySelect(categoryId: string){
     categoryId === category ? setCategory('') : setCategory(categoryId);
   }
 
-  function handleAppointmentDetails(){
-    navigation.navigate('AppointmentDetails');
+  function handleAppointmentDetails(guildSelected: AppointmentProps){
+    navigation.navigate('AppointmentDetails',{ guildSelected });
   }
   
   function handleAppointmentCreate(){
     navigation.navigate('AppointmentCreate');
   }
+
+  async function loadAppointments() {
+    const response = await AsyncStorage.getItem(COLLECTION_APPOINTMENTS);
+    const storage: AppointmentProps[] = response ? JSON.parse(response) : [];
+
+    if(category){
+      setAppointments(storage.filter(item => item.category === category));
+    } else {
+      setAppointments(storage);
+    }
+
+    setLoading(false);
+  }
+
+  useFocusEffect(useCallback( () => {
+    loadAppointments();
+  },[category]));
 
   return(
     <Background>
@@ -127,26 +62,34 @@ export function Home() {
         categorySelected={category}
         setCategory={handleCategorySelect}
       />
+      
+      {
+        loading
+        ? <Load />
+        : (
+          <>
+            <ListHeader 
+              title="Partidas agendadas"
+              subtitle={`Total ${appointments.length}`}
+            />
 
-      <ListHeader 
-        title="Partidas agendadas"
-        subtitle="Total 6"
-      />
-
-      <FlatList 
-        data={appointments}
-        keyExtractor={item => item.id}
-        showsVerticalScrollIndicator={false}
-        style={styles.matches}
-        ItemSeparatorComponent={() => <ListDivider />}
-        contentContainerStyle={{ paddingBottom: 69 }}
-        renderItem={({ item }) => (
-          <Appointment 
-            data={item}
-            onPress={handleAppointmentDetails}
-          />
-        )}
-      />
+            <FlatList 
+              data={appointments}
+              keyExtractor={item => item.id}
+              showsVerticalScrollIndicator={false}
+              style={styles.matches}
+              ItemSeparatorComponent={() => <ListDivider />}
+              contentContainerStyle={{ paddingBottom: 69 }}
+              renderItem={({ item }) => (
+                <Appointment 
+                  data={item}
+                  onPress={() => handleAppointmentDetails(item)}
+                />
+              )}
+            />
+          </>  
+        )
+      }
     </Background>
   );
 }
